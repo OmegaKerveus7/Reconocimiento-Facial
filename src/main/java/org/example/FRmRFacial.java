efe3c52c3667b56c8f7dbf05e00fe99e5b1b3ad4
Gustavo Adolfo Tobías Ramírez <tobiasgusito@outlook.com>
2025-10-27 20:16:01 -0600

    Projecto de Javier

---
+++ src/main/java/org/example/FRmRFacial.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javax.swing.*;
+
+public class FRmRFacial {
+    private JPanel JpanelP;
+    private JButton btnLogin;
+    private JTextField textField1;
+    private JButton tomarFotoButton;
+
+    public JPanel getPanel() {
+        return JpanelP;
+    }
+
+
+    public static void main(String[] args) {
+        FRmRFacial ventanaPrincipal = new FRmRFacial();
+        JFrame ventana = new JFrame("Ventana Principal");
+        ventana.setContentPane(ventanaPrincipal.getPanel()); // Aquí se usa el panel de la misma instancia
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.pack();
+        ventana.setVisible(true);
+        ventana.setSize(1000,800);
+    }
+}
